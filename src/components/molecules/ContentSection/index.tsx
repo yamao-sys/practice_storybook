7036b93d6b47430a801244940f87fc6638f9aadd
yamao-sys <yamaopanku@gmail.com>
2024-05-13 09:52:18 +0900

    再び動作検証のためレイアウトの変更

--- src/components/molecules/ContentSection/index.tsx
+++ src/components/molecules/ContentSection/index.tsx
@@ -15,6 +15,6 @@ export const ContentSection = ({ children }: Props) => {
 }
 
 const Wrapper = styled.div`
-	margin: ${({ theme }) => theme.size.p10};
+	margin: ${({ theme }) => theme.size.p20};
 `;
 Wrapper.defaultProps = { theme: theme };
